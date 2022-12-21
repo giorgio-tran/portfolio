4e39febd5d4fc137b833b3cab5df43ac2b4b7400
Giorgio <tran.giorgio@gmail.com>
2022-12-21 09:54:51 -1000

    Implement first section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,29 @@
+import { Link } from 'gatsby-link';
 import React from 'react';
 
-function IndexPage() {
-  return (
-    <main>
-      <section className="bg-slate-600">
-        <h1 className="">Hello World!</h1>
-
+const IndexPage = () => (
+  <main className="min-h-screen flex flex-col align-middle w-screen">
+    <div className="w-full max-w-7xl flex flex-col align-middle mx-auto px-3">
+      <section className="flex w-full justify-between">
+        <div className="flex flex-col align-middle justify-center">
+          <h1 className="text-6xl md:text-9xl p-0">Giorgio Tran</h1>
+          <p className="text-center pt-2 text-lg">
+            <b>MSCS Student @ UHM</b>
+          </p>
+        </div>
+        <div className="flex flex-col justify-center text-4xl">
+          <Link to="/404">LinkedIn</Link>
+          <Link to="/404">GitHub</Link>
+          <Link to="/404">Email</Link>
+        </div>
       </section>
       <section className="bg-slate-50">
         <h1>Testing</h1>
       </section>
-    </main>
-  );
-}
+    </div>
+  </main>
+);
 
 export default IndexPage;
 
-export function Head() {
-  return <title>Giorgio Tran</title>;
-}
+export const Head = () => <title>Giorgio Tran</title>;
