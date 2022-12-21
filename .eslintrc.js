1f6ea2b431c53c0b80dd1aca91cb46676cc9bd99
Giorgio <tran.giorgio@gmail.com>
2022-12-21 09:53:59 -1000

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,10 @@ module.exports = {
     __PATH_PREFIX__: true,
   },
   extends: 'airbnb',
+  rules: {
+    'func-names': 'off',
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/no-array-index-key': 'off',
+  },
 };
