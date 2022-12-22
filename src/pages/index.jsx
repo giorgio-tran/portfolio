773c36ef811b000967b3e75ab67662fc2e4a1d9e
Giorgio <tran.giorgio@gmail.com>
2022-12-21 22:59:15 -1000

    Add about me and projects section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,8 @@ import LinkNewTab from '../components/LinkNewTab';
 
 const IndexPage = () => (
   <main className="min-h-screen flex flex-col align-middle w-screen">
-    <div className="w-full max-w-6xl flex flex-col align-middle mx-auto px-3">
-      <section className="flex w-full justify-between">
+    <div className="w-full max-w-6xl flex flex-col align-middle mx-auto px-5">
+      <section className="flex w-full justify-between pb-10 md:pb-72">
         <div className="flex flex-col align-middle justify-center">
           <h1 className="text-6xl md:text-9xl p-0">Giorgio</h1>
           <h1 className="text-6xl md:text-9xl p-0">Tran</h1>
@@ -12,11 +12,21 @@ const IndexPage = () => (
         <div className="flex flex-col justify-end text-3xl md:text-6xl md:gap-2">
           <LinkNewTab link="https://linkedin.com/in/giorgio-tran-03a656139/" text="LinkedIn" />
           <LinkNewTab link="https://github.com/giorgio-tran/" text="GitHub" />
-          <a href="/404">Resume</a>
+          <LinkNewTab link="#" text="Resume" />
+        </div>
+      </section>
+      <section className="pb-10 md:pb-72">
+        <h1 className="w-full border-b-2 border-b-black text-3xl md:text-6xl pb-2">
+          ABOUT ME
+        </h1>
+        <div className="py-2 flex flex-col gap-1 text-2xl">
+          <p>Master&apos;s in Computer Science 2024.</p>
+          <p>Keyboard Enthusiast.</p>
+          <p>Coffee Aficionado.</p>
         </div>
       </section>
       <section>
-        <h1>Testing</h1>
+        <h1 className="border-b-2 border-b-black text-3xl md:text-6xl pb-2">PROJECTS</h1>
       </section>
     </div>
   </main>
