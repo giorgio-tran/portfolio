f899163b380470c0589bfee6315d53c2d323836d
Giorgio <tran.giorgio@gmail.com>
2022-12-24 21:30:00 -1000

    Improve animation and add stock project card

---
+++ src/components/StockProjectCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Pill from './Pill';
+
+const StockProjectCard = ({ project }) => (
+  <div className="w-full py-10 grid md:grid-cols-6 md:gap-10">
+    <div className="md:col-span-2 flex flex-auto pb-2">
+      <img className="w-full h-full object-contain" alt="project" src={project.image} />
+    </div>
+    <div className="md:col-span-4 flex flex-col gap-3">
+      <h1 className="text-3xl">{project.title}</h1>
+      <div className="flex gap-2 flex-wrap">
+        {project.tech.map((data) => (<Pill data={data} />))}
+      </div>
+      <p className="text-xl">{project.description}</p>
+    </div>
+  </div>
+);
+
+StockProjectCard.propTypes = {
+  project: PropTypes.shape({
+    title: PropTypes.string,
+    tech: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string,
+  }).isRequired,
+};
+
+export default StockProjectCard;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import LinkNewTab from '../components/LinkNewTab';
 import ProjectCard from '../components/ProjectCard';
 import projects from '../info/projects';
-import useFadeInOnScroll from '../hooks/useFadeInOnScroll';
+// import useFadeInOnScroll from '../hooks/useFadeInOnScroll';
 import useFadeInOnLoad from '../hooks/useFadeInOnLoad';
+import StockProjectCard from '../components/StockProjectCard';
 
 const IndexPage = () => {
-  const element = useFadeInOnScroll();
+  // const element = useFadeInOnScroll();
   const elementLoad = useFadeInOnLoad();
 
   return (
     <div className="min-h-screen flex flex-col align-middle w-full">
-      <div className="w-full max-w-6xl flex flex-col align-middle mx-auto px-5">
-        <div className="fade-in" ref={elementLoad}>
+      <div className="w-full max-w-screen-xl flex flex-col align-middle mx-auto px-5 fade-in" ref={elementLoad}>
+        <div>
           <section className="flex w-full justify-between pb-24 md:pb-72">
             <div className="flex flex-col align-middle justify-center">
               <h1 className="text-6xl md:text-9xl p-0">Giorgio</h1>
@@ -36,8 +37,9 @@ const IndexPage = () => {
           </section>
         </div>
         <section className="pb-10 md:pb-72">
-          <h1 className="border-b-2 border-b-black text-3xl md:text-6xl pb-2 fade-in" ref={element}>PROJECTS</h1>
-          {projects.map((project) => (<ProjectCard project={project} />))}
+          <h1 className="border-b-2 border-b-black text-3xl md:text-6xl pb-2">PROJECTS</h1>
+          <StockProjectCard project={projects[0]} />
+          {projects.slice(1).map((project) => (<ProjectCard project={project} />))}
         </section>
       </div>
       <footer className="w-full max-w-6xl flex flex-col align-middle mx-auto px-5 pb-10">
